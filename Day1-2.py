6d2feb009483f6ddaf0ac1b2583bbf7c3d7775ab
E'lo <elopus001@gmail.com>
2025-12-06 12:48:49 -0700

    Update Day1-2.py
    Remove global variable to maybe fix linter error

--- Day1-2.py
+++ Day1-2.py
@@ -1,42 +1,47 @@
-global zeros
-
 def rleft(i):
+    zeros = 0
     o = i - 1
     if o == -1:
         o = 99
     if o == 0:
         #print("Zero")
         zeros += 1
-    return o
+    return o, zeros
     
 def rright(i):
+    zeros = 0
     o = i + 1
     if o == 100:
         o = 0
     if o == 0:
         #print("Zero")
         zeros += 1
-    return o
+    return o,zeros
 
 def rotate(i, line):
+    zeros = 0
+    newzeros = 0
     o = i
     if line[0] == "L":
         i = 0
         while i < int(line[1:]):
-            o = rleft(o)
+            o, newzeros = rleft(o)
+            zeros += newzeros
             i += 1
     elif line[0] == "R":
         i = 0
         while i < int(line[1:]):
-            o = rright(o)
+            o, newzeros = rright(o)
+            zeros += newzeros
             i += 1
     else:
         print("Error, not a valid command.")
     
-    return o
+    return o, zeros
 
 def main():
     zeros = 0
+    newzeros = 0
     r = 50
     print(r)
     print("")
@@ -45,7 +50,8 @@ def main():
     content = file.read()
     contents = content.split("\n")
     for f in contents:
-        r = rotate(r, f)
+        r, newzeros = rotate(r, f)
+        zeros += newzeros
         print(f + " " + str(r))
         #print("")
     print(zeros)
